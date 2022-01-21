d61aefa6619e4a3fb70b0a7fa0e1f3a8cb4ce540
Mariano Cano <mariano.cano@gmail.com>
2022-01-20 19:19:01 -0800

    Add query builder code.

---
+++ query_builder.go
@@ -0,0 +1,113 @@
+package qb
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	idColumn        = "id"
+	createdAtColumn = "created_at"
+)
+
+// QueryBuilder provides a simple list of SQL queries that can be used by the
+// models. It requires tables with the columns id, created_at, and deleted_at.
+type QueryBuilder struct {
+	Table         string
+	Columns       []string
+	SelectDeleted bool
+}
+
+// NewQueryBuilder returns a new query builder configured with the given table
+// and columns.
+func NewQueryBuilder(table string, columns []string) *QueryBuilder {
+	return &QueryBuilder{
+		Table:         table,
+		Columns:       columns,
+		SelectDeleted: false,
+	}
+}
+
+// Queries returns the queries for select by id, insert,
+// update, and delete.
+func (q *QueryBuilder) Queries() (string, string, string, string) {
+	return q.Select(), q.Insert(), q.Update(), q.Delete()
+}
+
+// Select returns the query to get a record by id.
+func (q *QueryBuilder) Select() string {
+	s := fmt.Sprintf("SELECT %s FROM %s WHERE id = $1", q.columns(), q.Table)
+	if !q.SelectDeleted {
+		s += " AND deleted_at IS NULL"
+	}
+	return s
+}
+
+// SelectBy returns a query to get a record by the given column name.
+func (q *QueryBuilder) SelectBy(name string, extraNames ...string) string {
+	s := fmt.Sprintf("SELECT %s FROM %s WHERE %s = $1", q.columns(), q.Table, name)
+	// Append extra names.
+	for i, n := range extraNames {
+		s += fmt.Sprintf(" AND %s = $%d", n, i+2)
+	}
+	if !q.SelectDeleted {
+		s += " AND deleted_at IS NULL"
+	}
+	return s
+}
+
+// Insert returns the query to insert an record.
+func (q *QueryBuilder) Insert() string {
+	return fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", q.Table, q.columns(), q.values())
+}
+
+// InsertWithReturning returns the query to insert that returns the id.
+func (q *QueryBuilder) InsertWithReturning() string {
+	var pos = 1
+	var columns, values []string
+	for _, name := range q.Columns {
+		if name != idColumn {
+			columns = append(columns, name)
+			values = append(values, "$"+strconv.Itoa(pos))
+			pos++
+		}
+	}
+	return fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s) RETURNING id", q.Table, join(columns), join(values))
+}
+
+// Update returns the query to update an record. Update won't update neither the
+// id nor the created_at column.
+func (q *QueryBuilder) Update() string {
+	var v []string
+	pos := 1
+	for _, name := range q.Columns {
+		if name != idColumn && name != createdAtColumn {
+			v = append(v, name+" = $"+strconv.Itoa(pos))
+			pos++
+		}
+	}
+	return fmt.Sprintf("UPDATE %s SET %s WHERE id = $%d", q.Table, join(v), pos)
+}
+
+// Delete returns the query to mark a record as deleted.
+func (q *QueryBuilder) Delete() string {
+	return fmt.Sprintf("UPDATE %s SET deleted_at = $1 WHERE id = $2", q.Table)
+}
+
+func (q *QueryBuilder) columns() string {
+	return strings.Join(q.Columns, ", ")
+}
+
+func (q *QueryBuilder) values() string {
+	n := len(q.Columns)
+	c := make([]string, n)
+	for i := 0; i < n; i++ {
+		c[i] = "$" + strconv.Itoa(i+1)
+	}
+	return join(c)
+}
+
+func join(s []string) string {
+	return strings.Join(s, ", ")
+}
---
+++ query_builder_test.go
@@ -0,0 +1,138 @@
+package qb
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewQueryBuilder(t *testing.T) {
+	type args struct {
+		table   string
+		columns []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *QueryBuilder
+	}{
+		{"ok", args{"users", []string{"id", "name", "email"}}, &QueryBuilder{Table: "users", Columns: []string{"id", "name", "email"}, SelectDeleted: false}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewQueryBuilder(tt.args.table, tt.args.columns); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewQueryBuilder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestQueryBuilder_Queries(t *testing.T) {
+	type fields struct {
+		Table         string
+		Columns       []string
+		SelectDeleted bool
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+		want1  string
+		want2  string
+		want3  string
+	}{
+		{"selectDeleted", fields{"users", []string{"id", "name", "email", "created_at", "deleted_at"}, true},
+			"SELECT id, name, email, created_at, deleted_at FROM users WHERE id = $1",
+			"INSERT INTO users (id, name, email, created_at, deleted_at) VALUES ($1, $2, $3, $4, $5)",
+			"UPDATE users SET name = $1, email = $2, deleted_at = $3 WHERE id = $4",
+			"UPDATE users SET deleted_at = $1 WHERE id = $2"},
+		{"noSelectDeleted", fields{"users", []string{"id", "name", "email", "created_at", "deleted_at"}, false},
+			"SELECT id, name, email, created_at, deleted_at FROM users WHERE id = $1 AND deleted_at IS NULL",
+			"INSERT INTO users (id, name, email, created_at, deleted_at) VALUES ($1, $2, $3, $4, $5)",
+			"UPDATE users SET name = $1, email = $2, deleted_at = $3 WHERE id = $4",
+			"UPDATE users SET deleted_at = $1 WHERE id = $2"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			q := &QueryBuilder{
+				Table:         tt.fields.Table,
+				Columns:       tt.fields.Columns,
+				SelectDeleted: tt.fields.SelectDeleted,
+			}
+			got, got1, got2, got3 := q.Queries()
+			if got != tt.want {
+				t.Errorf("QueryBuilder.Queries() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("QueryBuilder.Queries() got1 = %v, want %v", got1, tt.want1)
+			}
+			if got2 != tt.want2 {
+				t.Errorf("QueryBuilder.Queries() got2 = %v, want %v", got2, tt.want2)
+			}
+			if got3 != tt.want3 {
+				t.Errorf("QueryBuilder.Queries() got3 = %v, want %v", got3, tt.want3)
+			}
+		})
+	}
+}
+
+func TestQueryBuilder_SelectBy(t *testing.T) {
+	type fields struct {
+		Table         string
+		Columns       []string
+		SelectDeleted bool
+	}
+	type args struct {
+		name       string
+		extraNames []string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{"selectDeleted", fields{"users", []string{"id", "name", "email", "created_at", "deleted_at"}, true}, args{"email", nil}, "SELECT id, name, email, created_at, deleted_at FROM users WHERE email = $1"},
+		{"noSelectDeleted", fields{"users", []string{"id", "name", "email", "created_at", "deleted_at"}, false}, args{"email", nil}, "SELECT id, name, email, created_at, deleted_at FROM users WHERE email = $1 AND deleted_at IS NULL"},
+		{"extra names", fields{"users", []string{"id", "name", "email", "created_at", "deleted_at"}, false}, args{"name", []string{"email"}}, "SELECT id, name, email, created_at, deleted_at FROM users WHERE name = $1 AND email = $2 AND deleted_at IS NULL"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			q := &QueryBuilder{
+				Table:         tt.fields.Table,
+				Columns:       tt.fields.Columns,
+				SelectDeleted: tt.fields.SelectDeleted,
+			}
+			if got := q.SelectBy(tt.args.name, tt.args.extraNames...); got != tt.want {
+				t.Errorf("QueryBuilder.SelectBy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestQueryBuilder_InsertWithReturning(t *testing.T) {
+	type fields struct {
+		Table         string
+		Columns       []string
+		SelectDeleted bool
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{"ok", fields{"users", []string{"id", "name", "email"}, false}, "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id"},
+		{"ok no id", fields{"users", []string{"name", "email"}, false}, "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			q := &QueryBuilder{
+				Table:         tt.fields.Table,
+				Columns:       tt.fields.Columns,
+				SelectDeleted: tt.fields.SelectDeleted,
+			}
+			if got := q.InsertWithReturning(); got != tt.want {
+				t.Errorf("QueryBuilder.InsertWithReturning() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
